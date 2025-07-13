f8f1bdca0035d6f2ec227fcee2e7b8d441b06eb2
Ibraheem Abdul-rahman <ibraheemabdulrahman157@gmail.com>
2025-07-13 11:26:02 +0100

    refactor: changed tailwind bg styling to inline css

--- src/components/WhatWeOffer.jsx
+++ src/components/WhatWeOffer.jsx
@@ -2,38 +2,57 @@ import { useState } from "react";
 
 function WhatWeOffer() {
   const [slide, setSlide] = useState(0);
-  const backgrounds = ['verified_artisans', 'marketplace', 'customer_support','localized_product','inclusive_onboarding','in_app_wallet'];
-  const title = ['Verified Artisans', 'A Marketplace', 'Great Customer support system','Localized product thinking','Inclusive onboarding','In App Wallet'];
-  const content = [
-    'We verify artisans through a structured onboarding process to ensure reliability, safety, and professionalism.',
-    'A list of trustworthy, recommended dealers to buy artisan-recommended tools, parts, and home essentials directly within the app',
-    'Fast-response bot with human fallback',
-    'Voice notes, local languages, intuitive interfaces for low-literacy users.',
-    'Freemium access for artisans and tiered access to more tools.',
-    'A secure digital wallet that lets users pay, store balance, and track transactions, all in one place.'
+
+  const backgrounds = [
+    "verified_artisans",
+    "marketplace",
+    "customer_support",
+    "localized_product",
+    "inclusive_onboarding",
+    "in_app_wallet"
   ];
 
-  let slides = []
-  for(let i = 0;i < backgrounds.length;i++){
-    slides.push(i)
-  }
+  const title = [
+    "Verified Artisans",
+    "A Marketplace",
+    "Great Customer support system",
+    "Localized product thinking",
+    "Inclusive onboarding",
+    "In App Wallet"
+  ];
 
+  const content = [
+    "We verify artisans through a structured onboarding process to ensure reliability, safety, and professionalism.",
+    "A list of trustworthy, recommended dealers to buy artisan-recommended tools, parts, and home essentials directly within the app",
+    "Fast-response bot with human fallback",
+    "Voice notes, local languages, intuitive interfaces for low-literacy users.",
+    "Freemium access for artisans and tiered access to more tools.",
+    "A secure digital wallet that lets users pay, store balance, and track transactions, all in one place."
+  ];
+
+  const slides = backgrounds.map((_, i) => i);
 
   function changeSlide(steps) {
-    setSlide(prev => (prev + steps + backgrounds.length) % backgrounds.length);
+    setSlide((prev) => (prev + steps + backgrounds.length) % backgrounds.length);
   }
-
 
   const mainSlide = slide;
   const nextSlide = (slide + 1) % backgrounds.length;
   const afterNext = (slide + 2) % backgrounds.length;
 
   return (
-    <section className={`my-16 py-16 px-8 md:pl-16 md:pr-0 flex gap-16 flex-col bg-[url(images/${backgrounds[mainSlide]}_bg.jpg)] bg-[var(--bg-green)] bg-blend-multiply bg-cover bg-center`}>
+    <section
+      className="my-16 py-16 px-8 md:pl-16 md:pr-0 flex gap-16 flex-col bg-[var(--bg-green)] bg-blend-multiply bg-cover bg-center"
+      style={{
+        backgroundImage: `url(/Images/${backgrounds[mainSlide]}_bg.jpg)`
+      }}
+    >
       <h1 className="font-bold text-[var(--color-secondary-400)]">What We Offer</h1>
       <div className="flex gap-8 items-center w-full">
         <div className="flex flex-col gap-8 w-2/5">
-          <h2 className="text-3xl font-bold text-[var(--color-secondary-400)]">{title[mainSlide]}</h2>
+          <h2 className="text-3xl font-bold text-[var(--color-secondary-400)]">
+            {title[mainSlide]}
+          </h2>
           <p className="text-[var(--color-secondary-400)]">{content[mainSlide]}</p>
           <button className="cursor-pointer mt-12 border bg-[var(--text-active)] w-fit text-background-dark py-2.5 px-8 font-medium border-none rounded-3xl justify-self-end">
             Get started
@@ -41,18 +60,42 @@ function WhatWeOffer() {
         </div>
 
         <div className="flex gap-4 items-baseline w-3/5 overflow-hidden">
-          <div className={`shrink-0 w-91.5 border-white rounded-4xl border-3 h-102.5 bg-[url(images/${backgrounds[mainSlide]}.jpg)] bg-cover bg-center`} />
-          <div className={`shrink-0 w-61  rounded-4xl h-75.5 bg-[url(images/${backgrounds[nextSlide]}.jpg)] bg-cover bg-center`} onClick={() => changeSlide(1)} />
-          <div className={`shrink-0 w-61  rounded-4xl h-75.5 bg-[url(images/${backgrounds[afterNext]}.jpg)] bg-cover bg-center`} onClick={() => changeSlide(2)} />
+          <div
+            className="shrink-0 w-91.5 border-white rounded-4xl border-3 h-102.5 bg-cover bg-center"
+            style={{
+              backgroundImage: `url(/Images/${backgrounds[mainSlide]}.jpg)`
+            }}
+          />
+          <div
+            className="shrink-0 w-61 rounded-4xl h-75.5 bg-cover bg-center cursor-pointer"
+            style={{
+              backgroundImage: `url(/Images/${backgrounds[nextSlide]}.jpg)`
+            }}
+            onClick={() => changeSlide(1)}
+          />
+          <div
+            className="shrink-0 w-61 rounded-4xl h-75.5 bg-cover bg-center cursor-pointer"
+            style={{
+              backgroundImage: `url(/Images/${backgrounds[afterNext]}.jpg)`
+            }}
+            onClick={() => changeSlide(2)}
+          />
         </div>
       </div>
 
       <form className="flex gap-4 self-center mt-8">
-        {slides.map(e => {
-          return  (<label htmlFor={`slide${e}`} key={e} className={`w-5 aspect-square rounded-full ${e == slide? `bg-[var(--text-active)]` : `bg-white`}`} onClick={()=> setSlide(e)}>
-            <input type="radio" name="slider" className="hidden" id={`slide${e}`} /> 
-          </label>)
-        })}
+        {slides.map((e) => (
+          <label
+            htmlFor={`slide${e}`}
+            key={e}
+            className={`w-5 aspect-square rounded-full ${
+              e === slide ? "bg-[var(--text-active)]" : "bg-white"
+            }`}
+            onClick={() => setSlide(e)}
+          >
+            <input type="radio" name="slider" className="hidden" id={`slide${e}`} />
+          </label>
+        ))}
       </form>
     </section>
   );
--- src/components/WhoSection.jsx
+++ src/components/WhoSection.jsx
@@ -1,12 +1,25 @@
 function WhoSection() {
   return (
     <section className="py-16 px-8 md:px-16 flex gap-6 md:gap-12 flex-col md:items-center">
-        <h1 className="md:text-center text-3xl font-bold">Who we are</h1>
-        <div className="flex flex-col md:flex-row items-center justify-between gap-2">
-            <p className="inline-flex w-full md:w-[30vw] text-base/loose tracking-wide md:text-justify">We’re a Servi-Tech platform that connects Nigerians to trusted, verified artisans, plumbers, electricians, painters, and more. We’re here to take the guesswork out of home services.</p>
-            <div className="aspect-square inline-flex w-[300px] border-12 border-[var(--text-active)] rounded-full bg-[url(images/whosection_image.png)] bg-cover bg-position-[0px_-30px]"></div>
-            <p className="inline-flex w-full md:w-[30vw] text-base/loose tracking-wide md:text-justify">We are offering speed, security, and reliability at your fingertips, while empowering artisans with steady jobs, visibility, and secure payments. At Sprintfixer it’s not just about connecting; we’re here to restore trust in the process.</p>        
-        </div>
+      <h1 className="md:text-center text-3xl font-bold">Who we are</h1>
+
+      <div className="flex flex-col md:flex-row items-center justify-between gap-2">
+        <p className="inline-flex w-full md:w-[30vw] text-base/loose tracking-wide md:text-justify">
+          We’re a Servi-Tech platform that connects Nigerians to trusted, verified artisans, plumbers, electricians, painters, and more. We’re here to take the guesswork out of home services.
+        </p>
+
+        <div
+          className="aspect-square inline-flex w-[300px] border-12 border-[var(--text-active)] rounded-full bg-cover"
+          style={{
+            backgroundImage: `url(/Images/whosection_image.png)`,
+            backgroundPosition: '0px -30px'
+          }}
+        ></div>
+
+        <p className="inline-flex w-full md:w-[30vw] text-base/loose tracking-wide md:text-justify">
+          We are offering speed, security, and reliability at your fingertips, while empowering artisans with steady jobs, visibility, and secure payments. At Sprintfixer it’s not just about connecting; we’re here to restore trust in the process.
+        </p>
+      </div>
     </section>
   );
 }
